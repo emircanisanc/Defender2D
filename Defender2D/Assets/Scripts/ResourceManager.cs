66763469266e01e0bb6b25552b003580139beb46
Emircan Isanc <emircanisanc@gmail.com>
2023-05-12 12:56:23 +0300

    ui

--- Defender2D/Assets/Scripts/ResourceManager.cs
+++ Defender2D/Assets/Scripts/ResourceManager.cs
@@ -1,14 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceManager : MonoBehaviour {
     public static ResourceManager Instance { get; private set; }
+
+    public event EventHandler OnResourceAmountChanged;
     private Dictionary<ResourceTypeSO, int> resourceAmountDictionary;
 
     void Awake() {
         Instance = this;
-        
+
         resourceAmountDictionary = new Dictionary<ResourceTypeSO, int>();
         
         ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
@@ -17,7 +20,6 @@ public class ResourceManager : MonoBehaviour {
             resourceAmountDictionary[resourceType] = 0;
         }
 
-        TestLogResourceAmountDictionary();
     }
 
     void Update() {
@@ -27,14 +29,13 @@ public class ResourceManager : MonoBehaviour {
         }
     }
 
-    private void TestLogResourceAmountDictionary() {
-        foreach (ResourceTypeSO resourceType in resourceAmountDictionary.Keys) {
-            Debug.Log(resourceType.nameString + ": " + resourceAmountDictionary[resourceType]);
-        }
-    }
-
     public void AddResource(ResourceTypeSO resourceType, int amount) {
         resourceAmountDictionary[resourceType] += amount;
-        TestLogResourceAmountDictionary();
+
+        OnResourceAmountChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public int GetResourceAmount(ResourceTypeSO resourceType) {
+        return resourceAmountDictionary[resourceType];
     }
 }
