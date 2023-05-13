b4bd6bad57b79e9d33bd4ed58352b579a28e9005
Emircan Isanc <emircanisanc@gmail.com>
2023-05-13 09:31:27 +0300

    resource nodes

--- Defender2D/Assets/Scripts/ResourceGenerator.cs
+++ Defender2D/Assets/Scripts/ResourceGenerator.cs
@@ -3,19 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceGenerator : MonoBehaviour {
-    private BuildingTypeSO buildingType;
+    private ResourceGeneratorData resourceGeneratorData;
     private float timer;
     private float timerMax;
+
     void Awake() {
-        buildingType = GetComponent<BuildingTypeHolder>().buildingType;
-        timerMax = buildingType.resourceGeneratorData.timerMax;
+        resourceGeneratorData = GetComponent<BuildingTypeHolder>().buildingType.resourceGeneratorData;
+        timerMax = resourceGeneratorData.timerMax;
+    }
+
+    void Start() {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, resourceGeneratorData.resourceDetectionRadius);
+
+        int nearbyResourceCount = 0;
+
+        foreach (Collider2D hit in hits) {
+            if (hit.TryGetComponent<ResourceNode>(out var resourceNode)) {
+                // It's a resource node!
+                if(resourceNode.resourceType == resourceGeneratorData.resourceType) {
+                    // Same type!
+                    nearbyResourceCount++;
+                    if(resourceGeneratorData.maxResourceAmount == nearbyResourceCount) {
+                        // Reached to limit!
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (nearbyResourceCount == 0) {
+            // No resources nearby
+            enabled = false;
+        } else {
+            // Calculate generation speed by nearby resource amount
+            timerMax = (resourceGeneratorData.timerMax / 2f) +
+             resourceGeneratorData.timerMax *
+             (1 - (float)nearbyResourceCount / resourceGeneratorData.maxResourceAmount);
+        }
     }
 
     void Update() {
         timer -= Time.deltaTime;
         if(timer <= 0f) {
             timer += timerMax;
-            ResourceManager.Instance.AddResource(buildingType.resourceGeneratorData.resourceType, 1);
+            ResourceManager.Instance.AddResource(resourceGeneratorData.resourceType, 1);
         }
     }
 }
--- Defender2D/Assets/Scripts/ResourceGeneratorData.cs
+++ Defender2D/Assets/Scripts/ResourceGeneratorData.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class ResourceGeneratorData {
     public float timerMax;
     public ResourceTypeSO resourceType;
+    public float resourceDetectionRadius;
+    public int maxResourceAmount;
 }
