6747150c7b539b6c5231bad2f8031e3794eea1e8
Emircan Isanc <emircanisanc@gmail.com>
2023-05-24 15:28:16 +0300

    HealthSystem

---
+++ Defender2D/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HealthSystem : MonoBehaviour {
+    
+    public event EventHandler OnDamaged;
+    public event EventHandler OnDied;
+    [SerializeField] private int healthAmountMax;
+    private int healthAmount;
+    
+    void Awake() {
+        healthAmount = healthAmountMax;
+    }
+
+    public void Damage(int damageAmount) {
+        healthAmount -= damageAmount;
+        healthAmount = Mathf.Clamp(healthAmount, 0, healthAmountMax);
+
+        OnDamaged?.Invoke(this, EventArgs.Empty);
+
+        if (IsDead()) {
+            OnDied?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public void SetHealthAmountMax(int healthAmountMax, bool updateHealthAmount) {
+        this.healthAmountMax = healthAmountMax;
+
+        if (updateHealthAmount) {
+            healthAmount = healthAmountMax;
+        }
+    }
+
+    public bool IsDead() {
+        return healthAmount == 0;
+    }
+
+    public bool IsFullHealth() {
+        return healthAmount == healthAmountMax;
+    }
+
+    public int GetHealthAmount() {
+        return healthAmount;
+    }
+
+    public float GetHealthAmountNormalized() {
+        return (float)healthAmount / healthAmountMax;
+    }
+}
