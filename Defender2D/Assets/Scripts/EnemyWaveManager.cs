0a886ec7cdec6135872fd8ae31dae67f01e9c02b
Emircan Isanc <emircanisanc@gmail.com>
2023-05-25 14:54:07 +0300

    enemy wave manager

---
+++ Defender2D/Assets/Scripts/EnemyWaveManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyWaveManager : MonoBehaviour {
+    
+    public event EventHandler OnWaveNumberChanged;
+
+    private enum State {
+        WaitingToSpawnNextWave,
+        SpawningWave,
+    }
+
+    [SerializeField] private List<Transform> spawnPositionTransformList;
+    [SerializeField] private Transform nextWaveSpawnPositionTransform;
+    private int waveNumber;
+    private State state;
+    private float nextWaveSpawnTimer;
+    private float nextEnemySpawnTimer;
+    private int remainingEnemySpawnAmount;
+    Vector3 spawnPosition;
+    void Start() {
+        state = State.WaitingToSpawnNextWave;
+        SetSpawnPosition();
+        nextWaveSpawnTimer = 3f;
+    }
+
+    void Update() {
+        switch (state) {
+            case State.WaitingToSpawnNextWave:
+                nextWaveSpawnTimer -= Time.deltaTime;
+                if (nextWaveSpawnTimer < 0) {
+                    SpawnWave();
+                }
+                break;
+            case State.SpawningWave:
+                if (remainingEnemySpawnAmount > 0) {
+                    nextEnemySpawnTimer -= Time.deltaTime;
+                    if (nextEnemySpawnTimer < 0f) {
+                        nextEnemySpawnTimer = UnityEngine.Random.Range(0, 0.2f);
+                        Enemy.Create(spawnPosition + UtilsClass.GetRandomDir() * UnityEngine.Random.Range(0f, 10f));
+                        remainingEnemySpawnAmount--;
+
+                        if (remainingEnemySpawnAmount == 0) {
+                            state = State.WaitingToSpawnNextWave;
+                            SetSpawnPosition();
+                            nextWaveSpawnTimer = 10f;
+                        }   
+                    }
+                }
+                break;
+        }
+    }
+
+    private void SpawnWave() {
+        remainingEnemySpawnAmount = 5 + 3 * waveNumber;
+        state = State.SpawningWave;
+        waveNumber++;
+        OnWaveNumberChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void SetSpawnPosition() {
+        spawnPosition = spawnPositionTransformList[UnityEngine.Random.Range(0, spawnPositionTransformList.Count)].position;
+        nextWaveSpawnPositionTransform.position = spawnPosition;
+    }
+
+    public int GetWaveNumber() {
+        return waveNumber;
+    }
+
+    public float GetNextWaveSpawnTimer() {
+        return nextWaveSpawnTimer;
+    }
+
+    public Vector3 GetSpawnPosition() {
+        return  spawnPosition;
+    }
+}
